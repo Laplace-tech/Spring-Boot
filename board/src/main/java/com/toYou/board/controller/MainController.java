eb9b318543960c2e6813cf1189f8db150f1aa72f
Laplace <add28482848@gmail.com>
2024-10-12 19:04:55 +0900

    Add files via upload

---
+++ board/src/main/java/com/toYou/board/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.toYou.board.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+	public String index() {
+		
+	}
+	
+	
+}
---
+++ board/src/test/java/com/toYou/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toYou.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
